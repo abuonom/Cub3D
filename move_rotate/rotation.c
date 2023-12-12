869396b1ca44ea0b2df1677ebd084fd1e18d4fc3
Alessio Buonomo <alessio@Alessio.PC>
2023-12-12 15:16:36 +0100

    Update

--- move_rotate/rotation.c
+++ move_rotate/rotation.c
@@ -6,17 +6,14 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:46:22 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/12/07 18:16:28 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/12/07 22:38:01 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
-
-void	update_rotation(t_cub3d *cube)
+void	rotate_right(t_cub3d *cube, double rotSpeed, double oldDirX, double oldPlaneX)
 {
-	double rotSpeed;
-	double oldDirX;
-	double oldPlaneX;
+
 
 	rotSpeed = cube->frameTime * 3.0;
 	if (cube->player.cam_dir == 1)
@@ -28,6 +25,9 @@ void	update_rotation(t_cub3d *cube)
 		cube->player.planeX = cube->player.planeX * cos(-rotSpeed) - cube->player.planeY * sin(-rotSpeed);
 		cube->player.planeY = oldPlaneX * sin(-rotSpeed) + cube->player.planeY * cos(-rotSpeed);
 	}
+}
+void	rotate_left(t_cub3d *cube, double rotSpeed, double oldDirX, double oldPlaneX)
+{
 	if (cube->player.cam_dir == -1)
 	{
 		oldDirX = cube->player.dirX;
@@ -37,4 +37,15 @@ void	update_rotation(t_cub3d *cube)
 		cube->player.planeX = cube->player.planeX * cos(rotSpeed) - cube->player.planeY * sin(rotSpeed);
 		cube->player.planeY = oldPlaneX * sin(rotSpeed) + cube->player.planeY * cos(rotSpeed);
 	}
+}
+
+void	update_rotation(t_cub3d *cube)
+{
+	double rotSpeed;
+	double oldDirX;
+	double oldPlaneX;
+
+	rotSpeed = cube->frameTime * 3.0;
+	rotate_right(cube, rotSpeed, oldDirX, oldPlaneX);
+	rotate_left(cube, rotSpeed, oldDirX, oldPlaneX);
 }
