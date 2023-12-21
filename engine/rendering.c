a5323a5d090337095d343b5684e3874f1b687cd5
Alessio Buonomo <alessio@Alessio.PC>
2023-12-21 18:40:47 +0100

    SI VEDEEEEEE

--- engine/rendering.c
+++ engine/rendering.c
@@ -6,7 +6,7 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 16:52:00 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/12/20 20:09:54 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/12/21 18:36:14 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,24 +55,28 @@ void sort_sprites(t_cub3d *cub3d)
 	}
 }
 
-static void	draw_sprite_1(t_cub3d *cub3d, t_sprite spr)
+static void	draw_sprite_1(t_cub3d *cub3d, t_sprite *spr)
 {
-	printf("spr.trasf_y dentro draw: %f\n", spr.transf_y);
-	spr.spr_h = fabs(WIN_HEIGHT / spr.transf_y);
-	printf("spr.spr_h: %f\n", spr.spr_h);
-	spr.drawy[0] = -spr.spr_h / 2 + WIN_HEIGHT / 2;
-	if (spr.drawy[0] < 0)
-		spr.drawy[0] = 0;
-	spr.drawy[1] = spr.spr_h / 2 + WIN_HEIGHT / 2;
-	if (spr.drawy[1] >= WIN_HEIGHT)
-		spr.drawy[1] = WIN_HEIGHT - 1;
-	spr.spr_w = fabs((WIN_HEIGHT / spr.transf_y));
-	spr.drawx[0] = -spr.spr_w / 2 + spr.spr_screen_x;
-	if (spr.drawx[0] < 0)
-		spr.drawx[0] = 0;
-	spr.drawx[1] = spr.spr_w / 2 + spr.spr_screen_x;
-	if (spr.drawx[1] >= WIN_WIDTH)
-		spr.drawx[1] = WIN_WIDTH - 1;
+	spr->spr_h = fabs(WIN_HEIGHT / spr->transf_y);
+	spr->drawy[0] = -spr->spr_h / 2 + WIN_HEIGHT / 2;
+	if (spr->drawy[0] < 0)
+		spr->drawy[0] = 0;
+	spr->drawy[1] = spr->spr_h / 2 + WIN_HEIGHT / 2;
+	if (spr->drawy[1] >= WIN_HEIGHT)
+		spr->drawy[1] = WIN_HEIGHT - 1;
+	spr->spr_w = fabs((WIN_HEIGHT / spr->transf_y));
+	spr->drawx[0] = -spr->spr_w / 2 + spr->spr_screen_x;
+	if (spr->drawx[0] < 0)
+		spr->drawx[0] = 0;
+	spr->drawx[1] = spr->spr_w / 2 + spr->spr_screen_x;
+	if (spr->drawx[1] >= WIN_WIDTH)
+		spr->drawx[1] = WIN_WIDTH - 1;
+	printf("spr->spr_h: %d\n", spr->spr_h);
+	printf("spr->drawy[0]: %d\n", spr->drawy[0]);
+	printf("spr->drawy[1]: %d\n", spr->drawy[1]);
+	printf("spr->spr_w: %d\n", spr->spr_w);
+	printf("spr->drawx[0]: %d\n", spr->drawx[0]);
+	printf("spr->drawx[1]: %d\n", spr->drawx[1]);
 }
 
 static void	draw_sprite_2(t_cub3d *game, t_sprite *spr, t_sprite spr_print,
@@ -89,15 +93,19 @@ static void	draw_sprite_2(t_cub3d *game, t_sprite *spr, t_sprite spr_print,
 	{
 		tex[0] = (256 * (stripe - (-spr->spr_w / 2 + spr->spr_screen_x))
 				* game->sprite_text.egg1.width / spr->spr_w) / 256;
+		//printf("256 * (%d - (%d / 2 + %d)) * %d / %d) / 256\n", stripe, spr->spr_w, spr->spr_screen_x, game->sprite_text.egg1.width, spr->spr_w);
+		//printf("tex[0]: %d\n", tex[0]);
 		if (spr->transf_y > 0 && spr->transf_y < zbuff[stripe])
 		{
 			v = spr->drawy[0] - 1;
 			while (++v < spr->drawy[1])
 			{
-				printf("v: %d\n", v);
+				//printf("v: %d\n", v);
 				d = (v) * 256 - WIN_HEIGHT * 128 + spr->spr_h * 128;
+				//printf("d: %d\n", d);
 				tex[1] = ((d * game->sprite_text.egg1.height) / spr->spr_h) / 256;
-				color = get_pixel_sprite(game->sprite_text.egg1.img, tex[0], tex[1]);
+				//printf("tex[1]: %d\n", tex[1]);
+				color = get_pixel_sprite(&game->sprite_text.egg1.img, tex[0], tex[1]);
 				if (color & 0x0FFFFFFF)
 					my_mlx_pixel_put(&game->img, stripe, v, color);
 			}
@@ -118,23 +126,23 @@ void draw_sprites(t_cub3d *cub3d, double zbuffer)
 		{
 			spr.x = (cub3d->sprite[i].x) - cub3d->player.posY;
 			spr.y = (cub3d->sprite[i].y) - cub3d->player.posX;
-			printf("spr.x: %f\n", spr.x);
-			printf("spr.y: %f\n", spr.y);
+			printf("cub3d->sprite[i].distance: %f\n", cub3d->sprite[i].distance);
+			printf("1.0 / %f * %f - %f * %f\n", cub3d->player.planeY ,cub3d->player.dirX, cub3d->player.dirY, cub3d->player.planeX);
 			spr.inv_det = 1.0 / (cub3d->player.planeY
 					* cub3d->player.dirX
 					- cub3d->player.dirY * cub3d->player.planeX);
 			printf("spr.inv_det: %f\n", spr.inv_det);
-			spr.transf_x = spr.inv_det * (cub3d->player.dirY * spr.x
-					- cub3d->player.dirX * spr.y);
+			spr.transf_x = spr.inv_det * (cub3d->player.dirX * spr.x
+					- cub3d->player.dirY * spr.y);
 			printf("spr.transf_x: %f\n", spr.transf_x);
-			spr.transf_y = spr.inv_det * (-cub3d->player.planeY
+			spr.transf_y = spr.inv_det * (-cub3d->player.planeX
 					* spr.x
-					+ cub3d->player.planeX * spr.y);
+					+ cub3d->player.planeY * spr.y);
 			printf("spr.transf_y: %f\n", spr.transf_y);
 			spr.spr_screen_x = (((double)(WIN_WIDTH) / 2.) * (1.
 						+ spr.transf_x / spr.transf_y));
-			printf("spr.spr_screen_x: %f\n", spr.spr_screen_x);
-			draw_sprite_1(cub3d, spr);
+			printf("spr.spr_screen_x: %d\n", spr.spr_screen_x);
+			draw_sprite_1(cub3d, &spr);
 			draw_sprite_2(cub3d, &spr, cub3d->sprite[i], &zbuffer);
 			printf("\n");
 		}
--- move_rotate/movement.c
+++ move_rotate/movement.c
@@ -6,7 +6,7 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:46:22 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/12/20 19:11:02 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/12/21 18:40:06 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	forward_move(t_cub3d *cub3d, double moveSpeed)
 {
 if (cub3d->player.mov_dirY == 1)
 	{
-		if (cub3d->map[(int)(cub3d->player.posX + cub3d->player.dirX * moveSpeed)][(int)cub3d->player.posY] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)(cub3d->player.posX + cub3d->player.dirX * moveSpeed)][(int)cub3d->player.posY]))
 			cub3d->player.posX += cub3d->player.dirX * moveSpeed;
-		if (cub3d->map[(int)(cub3d->player.posX)][(int)(cub3d->player.posY + cub3d->player.dirY * moveSpeed)] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)(cub3d->player.posX)][(int)(cub3d->player.posY + cub3d->player.dirY * moveSpeed)]))
 			cub3d->player.posY += cub3d->player.dirY * moveSpeed;
 	}
 }
@@ -26,9 +26,9 @@ void	backward_move(t_cub3d *cub3d, double moveSpeed)
 {
 		if (cub3d->player.mov_dirY == -1)
 	{
-		if (cub3d->map[(int)(cub3d->player.posX - cub3d->player.dirX * moveSpeed)][(int)cub3d->player.posY] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)(cub3d->player.posX - cub3d->player.dirX * moveSpeed)][(int)cub3d->player.posY]))
 			cub3d->player.posX -= cub3d->player.dirX * moveSpeed;
-		if (cub3d->map[(int)cub3d->player.posX][(int)(cub3d->player.posY - cub3d->player.dirY * moveSpeed)] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)cub3d->player.posX][(int)(cub3d->player.posY - cub3d->player.dirY * moveSpeed)]))
 			cub3d->player.posY -= cub3d->player.dirY * moveSpeed;
 	}
 }
@@ -37,9 +37,9 @@ void	left_move(t_cub3d *cub3d, double moveSpeed)
 {
 		if (cub3d->player.mov_dirX == -1)
 	{
-		if (cub3d->map[(int)(cub3d->player.posX - cub3d->player.dirY * moveSpeed)][(int)cub3d->player.posY] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)(cub3d->player.posX - cub3d->player.dirY * moveSpeed)][(int)cub3d->player.posY]))
 			cub3d->player.posX -= cub3d->player.dirY * moveSpeed;
-		if (cub3d->map[(int)(cub3d->player.posX)][(int)(cub3d->player.posY + cub3d->player.dirX * moveSpeed)] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)(cub3d->player.posX)][(int)(cub3d->player.posY + cub3d->player.dirX * moveSpeed)]))
 			cub3d->player.posY += (cub3d->player.dirX) * moveSpeed;
 	}
 }
@@ -48,9 +48,9 @@ void	right_move(t_cub3d *cub3d, double moveSpeed)
 {
 		if (cub3d->player.mov_dirX == 1)
 	{
-		if (cub3d->map[(int)(cub3d->player.posX + cub3d->player.dirY * moveSpeed)][(int)cub3d->player.posY] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)(cub3d->player.posX + cub3d->player.dirY * moveSpeed)][(int)cub3d->player.posY]))
 			cub3d->player.posX += cub3d->player.dirY * moveSpeed;
-		if (cub3d->map[(int)cub3d->player.posX][(int)(cub3d->player.posY - cub3d->player.dirX * moveSpeed)] == '0')
+		if (ft_strrchr("02", cub3d->map[(int)cub3d->player.posX][(int)(cub3d->player.posY - cub3d->player.dirX * moveSpeed)]))
 			cub3d->player.posY -= (cub3d->player.dirX) * moveSpeed;
 	}
 }
